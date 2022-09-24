ac120379efde9161d49d5762aba4b3c2dd35b14f
al3alamyibrahim <54779488+al3alamyibrahim@users.noreply.github.com>
2022-09-24 03:57:12 +0200

    =laravel

---
+++ lang/ar/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'هذه المدخلات لا تطابق ايا من المدخلات المسجلة لدينا.',
+    'password' => 'كلمة المرور التي ادخلتها غير صحيحة.',
+    'throttle' => 'محاولات متعددة,حاول مجددا بعد :seconds ثانية.',
+    'Login' => 'تسجيل الدخول'
+
+];
